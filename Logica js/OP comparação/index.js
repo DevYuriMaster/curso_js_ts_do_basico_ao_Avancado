81bb4275efa81e4bce487745a2f988b020ff61fd
Yuri Victor <72678779+DevYuriMaster@users.noreply.github.com>
2022-03-11 18:52:03 -0300

    Lógica em JavaScript
    Exercícios e anotações do aprendizado de lógica em javascript

---
+++ Logica js/OP comparação/index.js
@@ -0,0 +1,17 @@
+/*Operadores de comparação
+---- 1. > Maior que
+---  2. >= Maior que ou iagual a 
+---  3. < Menor que 
+---  4. <= Menor que ou iagual a
+---  5. == iagualdade (valor)
+---  6. === igualdade estrita (valor e tipo)
+---  7. != Diferente (valor)
+---  8. !== Diferente estrito (valor e tipo)
+*/
+
+
+
+    
+   
+
+
