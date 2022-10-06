f79e2d2bf481d84ce3fef2df3bfef4fee111d2e1
Yuri Victor <72678779+DevYuriMaster@users.noreply.github.com>
2022-10-06 15:14:51 -0300

    Juntar Arrays

---
+++ Arrays(Avançado)/junção de Arrays/main.js
@@ -0,0 +1,13 @@
+//formas de juntar arrays
+/* -Função concat
+const a1 = [1, 2, 3];
+const a2 = [4, 5, 6];
+const a3 = a1.concat(a2);
+console.log(a3);
+*/
+
+// Forma com Spread 
+const a1 = [1, 2, 3];
+const a2 = [4, 5, 6];
+const a3 = [...a1, ...a2];
+console.log(a3);
