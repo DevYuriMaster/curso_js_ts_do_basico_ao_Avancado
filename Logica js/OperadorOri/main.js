674363e4e0304c3e998b676d64157591e6a8aece
Yuri Victor <72678779+DevYuriMaster@users.noreply.github.com>
2022-10-06 15:18:36 -0300

    Lógica

---
+++ Logica js/OperadorOri/main.js
@@ -0,0 +1,23 @@
+//some 500 ao valor do scroll abaixo, 
+//atribuindo novo valor a scroll
+
+var scroll = 1000
+var aumento = 500
+
+scroll += aumento;
+
+console.log(scroll);
+
+//Atribua true para a variável darCrédito,
+//caso o cliente possua carro e casa
+//E false caso o contrário
+
+var darCrédito;
+var possuiCarro = true;
+var possuiCasa = true;
+
+darCrédito = (possuiCarro && possuiCasa);
+
+
+
+
