a14b07871a62a24ad3e04ac69b0f189070023e65
Yuri Victor <yurivictor4525@gmail.com>
2024-03-07 19:41:26 -0300

    Fetch e Conceitos

---
+++ javascript B7WEB/Requisições/Fetch/script.js
@@ -0,0 +1,31 @@
+
+// 1 Realizando e tratando a requisição
+function loadPosts() {
+
+    document.getElementById("posts").innerHTML = 'Carregando...';
+
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(function (resultado) {
+            //2 - Transformar o resultado da requisição em formato JSON
+            return resultado.json() //3 - O resultado trará uma nova promisse
+        })
+        .then(function(json){
+           //document.getElementById("posts").innerHTML = json.length+" posts";
+           montarBlog(json);
+        })
+        .catch(function (error) {
+            console.log("Deu problema...")
+        });
+}
+
+function montarBlog(lista){
+    let html = '';
+
+    for(i in lista){
+        html += '<h3>' +lista[i].title+ '</h3>';
+        html += lista[i].body+'<br/>'
+        html += '<hr/>'; 
+    }
+
+    document.getElementById("posts").innerHTML = html;
+}
